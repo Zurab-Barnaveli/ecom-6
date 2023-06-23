6b6832c3ca94bf3d2f2e4a0891131a3685657d48
Zurab-Barnaveli <zuka.barnaveli11@gmail.com>
2023-06-23 17:04:27 +0400

    first commit

---
+++ ecom-web/src/components/navigation/Brands/Brands.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+import { useStore } from "../../../zustand/store";
+import { Box } from "@mui/material";
+import { useEffect } from "react";
+
+import "./Brands.scss";
+
+export default function Brands() {
+  const { fetchBrands, brands } = useStore();
+
+  useEffect(() => {
+    fetchBrands();
+  }, []);
+
+  return (
+    <Box>
+      <div className='dropdown'>
+        <button className='dropbtn'>Brands</button>
+        <div className='dropdown-content'>
+          {brands.map((brand) => (
+            <p key={brand}>{brand}</p>
+          ))}
+        </div>
+      </div>
+    </Box>
+  );
+}
